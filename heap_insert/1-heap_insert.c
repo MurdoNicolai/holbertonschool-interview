8aa68db3dc17d8c8e79c7ba600520231bf059edc
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-01 18:42:22 +0200

    fix segfault

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -21,25 +21,34 @@ heap_t *heap_insert(heap_t **root, int value)
 	}
 	new_node->n = value;
 	new_node->right = NULL;
+	new_node->left = NULL;
+	new_node->parent = NULL;
 	if ((*root) == NULL)
 	{
 		*root = new_node;
-		new_node->left = NULL;
 		return (*root);
 	}
 	if ((*root)->n < new_node->n)
 	{
 		new_node->left = *root;
+		new_node->left->parent = new_node;
 		*root = new_node;
 		return (*root);
 	}
 	current_node = (*root);
 	while (1)
 	{
-		if (current_node->left == NULL || current_node->left->n < new_node->n)
+		if (current_node->left == NULL){
+			current_node->left = new_node;
+			new_node->parent = current_node;
+			return (*root);
+		}
+		if (current_node->left->n < new_node->n)
 		{
 			new_node->left = current_node->left;
+			new_node->parent = current_node;
 			current_node->left = new_node;
+			new_node->left->parent = new_node;
 			return (*root);
 		}
 		current_node = current_node->left;
