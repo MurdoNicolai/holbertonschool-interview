acc86ba35970d64f6d9244da3ed90804882442b3
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-05 21:21:27 +0200

    try 2

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -6,9 +6,10 @@ def minOperations(n):
     """ returns the minimum number of operations to be
     performed to get n h by using copy all and paste only"""
     dividends = []
-    for i in range(2, n):
+    i = 2
+    while n > 1:
         if n % i == 0:
             dividends.append(i)
-    if len(dividends) == 0:
-        return ((n - 1) * 2)
-    return (sum(dividends) - 1) * 2
+            n = n/i
+        i += 1
+    return sum(dividends)
