b7d2030bbc21f7d7c17e6bc2d98969443cca9706
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-15 12:43:37 +0200

    no grid 1?

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -10,55 +10,61 @@
  */
 static void print_grid(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
 
 /**
 *	sandpile_tople- tople sandpile 1 givving grid 2 as result of that topling
-*	@grid1: first grid
 *	@grid2: empty grid
 *	Return: grid 2 to later add to grid 1 to get the result
 **/
 int (*sandpile_tople(int grid1[3][3], int grid2[3][3]))[3]
 {
 	int i, j;
+
 	for (i = 0; i < 3; i++) {
 			for (j = 0; j < 3; j++) {
-				if (grid1[i][j] > 3) {
-					if (i - 1  >= 0) {
-						if (grid1[i - 1][j] <= 3) {
+				if (grid1[i][j] > 3)
+				{
+					if (i - 1  >= 0)
+					{
+						if (grid1[i - 1][j] <= 3)
+						{
 							grid2[i][j] -= 1;
 							grid2[i - 1][j] += 1;
 						}
 					}
 					else grid2 [i][j] -= 1;
 					if (j - 1  >= 0) {
-						if (grid1[i][j - 1] <= 3) {
+						if (grid1[i][j - 1] <= 3)
+						{
 							grid2[i][j] -= 1;
 							grid2[i][j - 1] += 1;
 						}
 					}
 					else grid2 [i][j] -= 1;
 					if (i + 1  < 3) {
-						if (grid1[i + 1][j] <= 3) {
+						if (grid1[i + 1][j] <= 3)
+						{
 							grid2[i][j] -= 1;
 							grid2[i + 1][j] += 1;
 						}
 					}
 					else grid2 [i][j] -= 1;
 					if (j + 1  < 3) {
-						if (grid1[i][j + 1] <= 3) {
+						if (grid1[i][j + 1] <= 3)
+						{
 							grid2[i][j] -= 1;
 							grid2[i][j + 1] += 1;
 						}
@@ -82,6 +88,7 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 	int i, j;
 	int print = 0;
 	int stable = 1;
+
 	for (i = 0; i < 3; i++) {
 		for (j = 0; j < 3; j++) {
 			if (grid2[i][j] != 0) {
