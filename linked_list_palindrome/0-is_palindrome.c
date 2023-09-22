d85ef5967d8e57a56916e0b5a927890322a44972
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-22 14:40:45 +0200

    add list.h

---
+++ linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+*	insert_node - Insert a node in an ordered list
+*	@head: the head of the list
+*	@number: the number to insert
+*	Return: adress of the newly inserted node
+**/
+int is_palindrome(listint_t **head)
+{
+	int array[200] = {};
+	int i = 0;
+	listint_t *pos = *head;
+	while (pos) {
+		array[i] = pos->n;
+		pos = pos->next;
+	}
+	int arraylen = i;
+	while (i >= 0) {
+		if (array[i] != array[arraylen - i]){
+			return(0);
+		}
+	}
+	return(1);
+}
--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -25,7 +25,8 @@ static void print_grid(int grid[3][3])
 }
 
 /**
-*	sandpile_tople- tople sandpile 1 givving grid 2 as result of that topling
+*	sandpile_tople - tople sandpile 1 givving grid 2 as result of that topling
+*	@grid1: the grid to check
 *	@grid2: empty grid
 *	Return: grid 2 to later add to grid 1 to get the result
 **/
@@ -33,46 +34,59 @@ int (*sandpile_tople(int grid1[3][3], int grid2[3][3]))[3]
 {
 	int i, j;
 
-	for (i = 0; i < 3; i++) {
-			for (j = 0; j < 3; j++) {
-				if (grid1[i][j] > 3)
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] > 3)
+			{
+				if (i - 1 >= 0)
 				{
-					if (i - 1  >= 0)
+					if (grid1[i - 1][j] <= 3)
 					{
-						if (grid1[i - 1][j] <= 3)
-						{
-							grid2[i][j] -= 1;
-							grid2[i - 1][j] += 1;
-						}
+						grid2[i][j] -= 1;
+						grid2[i - 1][j] += 1;
 					}
-					else grid2 [i][j] -= 1;
-					if (j - 1  >= 0) {
-						if (grid1[i][j - 1] <= 3)
-						{
-							grid2[i][j] -= 1;
-							grid2[i][j - 1] += 1;
-						}
-					}
-					else grid2 [i][j] -= 1;
-					if (i + 1  < 3) {
-						if (grid1[i + 1][j] <= 3)
-						{
-							grid2[i][j] -= 1;
-							grid2[i + 1][j] += 1;
-						}
-					}
-					else grid2 [i][j] -= 1;
-					if (j + 1  < 3) {
-						if (grid1[i][j + 1] <= 3)
-						{
-							grid2[i][j] -= 1;
-							grid2[i][j + 1] += 1;
-						}
-					}
-					else grid2 [i][j] -= 1;
 				}
+				else
+					grid2[i][j] -= 1;
+
+				if (j - 1 >= 0)
+				{
+					if (grid1[i][j - 1] <= 3)
+					{
+						grid2[i][j] -= 1;
+						grid2[i][j - 1] += 1;
+					}
+				}
+				else
+					grid2[i][j] -= 1;
+
+				if (i + 1 < 3)
+				{
+					if (grid1[i + 1][j] <= 3)
+					{
+						grid2[i][j] -= 1;
+						grid2[i + 1][j] += 1;
+					}
+				}
+				else
+					grid2[i][j] -= 1;
+
+				if (j + 1 < 3)
+				{
+					if (grid1[i][j + 1] <= 3)
+					{
+						grid2[i][j] -= 1;
+						grid2[i][j + 1] += 1;
+					}
+				}
+				else
+					grid2[i][j] -= 1;
 			}
 		}
+	}
+
 	return (grid2);
 }
 
@@ -89,23 +103,30 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 	int print = 0;
 	int stable = 1;
 
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) {
-			if (grid2[i][j] != 0) {
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+
+			if (grid2[i][j] != 0)
+			{
 				stable = 0;
 				grid1[i][j] += grid2[i][j];
 				grid2[i][j] = 0;
 			}
-			if (grid1[i][j] > 3) {
+			if (grid1[i][j] > 3)
+			{
 				print = 1;
 			}
 		}
 	}
 
-	if (!stable) {
-		if (print){
-					printf("=\n");
-					print_grid(grid1);
+	if (!stable)
+	{
+		if (print)
+		{
+			printf("=\n");
+			print_grid(grid1);
 		}
 		sandpiles_sum(grid1, sandpile_tople(grid1, grid2));
 	}
