01d8d305bb8c2bbbe211bbb2040f33007fa237a4
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-10-06 12:44:12 +0200

    add checklist

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+*	insert_node - check if linked list has ciycle
+*	@head: the head of the list
+*	@number: the number to insert
+*	Return: 1 if cycle 0 otherwise
+**/
+int check_cycle(listint_t *list){
+	listint_t *head;
+	head = list;
+	while (1){
+		if (list == NULL) return 0;
+		if (list == head) return 1;
+		list = list->next;
+	}
+}
