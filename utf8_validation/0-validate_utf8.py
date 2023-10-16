02a912d17960b5b5de57b1239133754804f79819
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-10-16 17:15:26 +0200

    pycodestyle

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
 """ conatinas validUTF8"""
 
 
@@ -8,20 +8,20 @@ def validUTF8(data):
         return False
     continuation = 0
     for num in data:
-        num = num%256
+        num = num % 256
         if continuation != 0:
-            if num > 191 or num < 128: #over 1011 1111 or under 1000 0000
+            if num > 191 or num < 128:  # over 1011 1111 or under 1000 0000
                 return False
             else:
                 continuation -= 1
         else:
-            if num >= 128 and num <= 191: #under 1011 1111 and over 1000 0000
+            if num >= 128 and num <= 191:  # under 1011 1111 and over 1000 0000
                 return False
-            if num >= 192: #over 1100 0000
+            if num >= 192:  # over 1100 0000
                 continuation += 1
-            if num >= 224: #over 1110 0000
+            if num >= 224:  # over 1110 0000
                 continuation += 1
-            if num >= 240: #over 1111 0000
+            if num >= 240:  # over 1111 0000
                 continuation += 1
     if continuation == 0:
         return True
