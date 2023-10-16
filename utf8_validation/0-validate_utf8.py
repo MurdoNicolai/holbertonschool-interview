9c43f1426f902685f61ddbe87cf3ec4289ccf8c3
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-10-16 17:11:41 +0200

    move everything 1 bit and add modulo

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -4,25 +4,25 @@
 
 def validUTF8(data):
     """ checs if data is a valid utf8"""
+    print('{0:08b}'.format(127))
     if type(data) is not list:
         return False
     continuation = 0
     for num in data:
-        if num > 511: #over 1111 1111
-            return False
+        num = num%256
         if continuation != 0:
-            if num >= 383 or num < 256: #over 1011 1111 or under 1000 0000
+            if num > 191 or num < 128: #over 1011 1111 or under 1000 0000
                 return False
             else:
-                conitnuation -= 1
+                continuation -= 1
         else:
-            if num >= 256 and num < 383: #under 1011 1111 and over 1000 0000
+            if num >= 128 and num <= 191: #under 1011 1111 and over 1000 0000
                 return False
-            if num >= 383: #over 1100 0000
+            if num >= 192: #over 1100 0000
                 continuation += 1
-            if num >= 447: #over 1110 0000
+            if num >= 224: #over 1110 0000
                 continuation += 1
-            if num >= 479: #over 1111 0000
+            if num >= 240: #over 1111 0000
                 continuation += 1
     if continuation == 0:
         return True
