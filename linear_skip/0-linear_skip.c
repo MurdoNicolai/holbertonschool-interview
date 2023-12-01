664df4d126e79b9f9b8111b9ceb6d98cbf859ab4
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-12-01 15:01:00 +0100

    fix next is last

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -5,36 +5,44 @@
 
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
+	skiplist_t *next;
+
 	if (list == NULL)
 		return NULL;
-	skiplist_t *next;
+
 	while (list != NULL)
 	{
 		next = list->express;
-		printf("Value checked at index [%ld] = [%d]\n", list->index, list->n);
+		if(next == NULL)
+		{
+			next = list;
+			while (next->next != NULL)
+			{
+				next = next->next;
+			}
+
+			printf("Value found between indexes [%ld] and [%ld]\n", list->index, next->index);
+			while (list != next->next)
+			{
+				printf("Value checked at index [%ld] = [%d]\n", list->index, list->n);
+				if (list->n == value)
+					return list;
+				list = list->next;
+			}
+			return NULL;
+		}
+		printf("Value checked at index [%ld] = [%d]\n", next->index, next->n);
 		if (list->n == value)
 			return list;
-		// if(next == NULL)
-		// {
-		// 	printf("Value found between indexes [%ld] and [%ld]", list->index, );
-		// 	while (list != NULL)
-		// 	{
-		// 		list = list->next;
-		// 		printf("Value checked at index [%ld] = [%d]\n", list->index, list->n);
-		// 		if (list->n == value)
-		// 			return list;
-		// 	}
-		// 	return NULL;
-		// }
-		if((list->n < value && next->n >= value) || (next->express == NULL))
+		if(list->n < value && next->n >= value)
 		{
 			printf("Value found between indexes [%ld] and [%ld]\n", list->index, next->index);
 			while (list != next->next)
 			{
-				list = list->next;
 				printf("Value checked at index [%ld] = [%d]\n", list->index, list->n);
 				if (list->n == value)
 					return list;
+				list = list->next;
 			}
 			return NULL;
 		}
