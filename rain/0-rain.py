02c5a292180e2a568f77540b675a8787a1086c75
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-12-11 11:49:34 +0100

    add readme

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -15,18 +15,24 @@ def rain(walls):
     waterhights = []
     minwallhight = 0
 
+    #
     for wall in walls:
         if wall > minwallhight:
             minwallhight = wall
         left_wall_min.append(minwallhight)
     minwallhight = 0
+
     for wall in walls[::-1]:
         if wall > minwallhight:
             minwallhight = wall
         right_wall_min.append(minwallhight)
     right_wall_min = right_wall_min[::-1]
+
     for w in range(len(left_wall_min)):
         waterhights.append(min(right_wall_min[w], left_wall_min[w]))
+
     for w in range(len(waterhights)):
         waterhights[w] = max(0, waterhights[w] - walls[w])
+
+
     return(sum(waterhights))
