93c3667fac82e48124eb5d3138cb64b745b8cfee
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2024-01-03 19:39:01 +0100

    add binary search

---
+++ advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "search_algos.h"
+
+/**
+*	insert_node - Insert a node in an ordered list
+*	@array: pointer to the first element of the array to search in
+*	@size: number of elements in array
+*	@value: the value to search for
+*	Return:  index where value is located
+**/
+int advanced_binary(int *array, size_t size, int value)
+{
+
+	size_t i;
+	int half;
+	int result;
+
+	if (array == NULL)
+		return -1;
+	printf("Searching in array:");
+	for (i = 0; i < (size - 1); i++){
+		printf(" %d,", array[i]);
+	}
+	printf(" %d\n", array[i]);
+	half = (size + 1) / 2;
+	if (array[0] == value)
+		return 0;
+
+	if (array[half - 1] >= value) {
+		result = advanced_binary(array, half, value);
+		if (result == -1)
+			return -1;
+		return (result);
+	}
+
+	else{
+		if (size == 1)
+			return -1;
+		result = advanced_binary((array + half), size - half, value);
+		if (result == -1)
+			return -1;
+		return (result + half);
+	}
+
+}
---
+++ advanced_binary_search/search_algos.h
@@ -0,0 +1,7 @@
+#ifndef _BINARY_SEARCH_H_
+#define _BINARY_SEARCH_H_
+
+#include <stddef.h>
+int advanced_binary(int *array, size_t size, int value);
+
+#endif
