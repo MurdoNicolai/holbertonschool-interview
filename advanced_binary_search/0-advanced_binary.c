eda8cdd8974007ef81ca3bd2928cec5e59bf1785
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2024-01-03 19:42:54 +0100

    fix style

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -4,7 +4,7 @@
 #include "search_algos.h"
 
 /**
-*	insert_node - Insert a node in an ordered list
+*	advanced_binary - search valu in array
 *	@array: pointer to the first element of the array to search in
 *	@size: number of elements in array
 *	@value: the value to search for
@@ -18,29 +18,31 @@ int advanced_binary(int *array, size_t size, int value)
 	int result;
 
 	if (array == NULL)
-		return -1;
+		return (-1);
+
 	printf("Searching in array:");
-	for (i = 0; i < (size - 1); i++){
+	for (i = 0; i < (size - 1); i++)
 		printf(" %d,", array[i]);
-	}
 	printf(" %d\n", array[i]);
 	half = (size + 1) / 2;
 	if (array[0] == value)
-		return 0;
+		return (0);
 
-	if (array[half - 1] >= value) {
+	if (array[half - 1] >= value)
+	{
 		result = advanced_binary(array, half, value);
 		if (result == -1)
-			return -1;
+			return (-1);
 		return (result);
 	}
 
-	else{
+	else
+	{
 		if (size == 1)
-			return -1;
+			return (-1);
 		result = advanced_binary((array + half), size - half, value);
 		if (result == -1)
-			return -1;
+			return (-1);
 		return (result + half);
 	}
 
