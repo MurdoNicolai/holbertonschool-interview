70800a4f5c6e6dce29e0008da34e937291eb8f2a
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2024-04-05 23:21:44 +0200

    del comments

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -1,30 +1,25 @@
 #include <stdio.h>
 
 int wildcmp(char *s1, char *s2) {
-    // Base case: if both strings are empty, they are considered identical
     if (*s1 == '\0' && *s2 == '\0') {
         return 1;
     }
 
-    // If the current character in s2 is '*', it can replace any string
     if (*s2 == '*') {
-        // Continue to the next character in s2 to see if it can match the remaining of s1
         while (*s1 != '\0') {
             if (wildcmp(s1, s2 + 1)) {
                 return 1;
             }
             s1++;
         }
-        // If we reach here, it means the rest of s1 doesn't match s2, so return 0
         return wildcmp(s1, s2 + 1);
     }
 
-    // If the current characters in s1 and s2 match, continue to the next characters
+
     if (*s1 == *s2) {
         return wildcmp(s1 + 1, s2 + 1);
     }
 
-    // If the characters don't match, return 0
     return 0;
 }
 
